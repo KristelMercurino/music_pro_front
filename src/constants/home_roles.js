622378aca5d004706877fccdd019266a0cd16dc6
Kristel <kristel.mercurino@hotmail.com>
2023-05-26 16:25:51 -0400

    routes contants added

---
+++ src/constants/home_roles.js
@@ -0,0 +1,7 @@
+export const ROLE_REDIRECT = {
+    Cliente: "/",
+    Administrador: "/admin-home",
+    Contador: "/accountant",
+    Vendedor: "/seller-home",
+    Bodeguero: "/grocer-home",
+  };
